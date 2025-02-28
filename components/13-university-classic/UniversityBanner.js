bc48035b0108d3f97c37dc5cb9fccd76ebcae7f7
nsathiyakala <nsathiyakala6@gmail.com>
2025-02-28 15:17:35 +0530

    desktop images

--- components/13-university-classic/UniversityBanner.js
+++ components/13-university-classic/UniversityBanner.js
@@ -124,7 +124,175 @@ const UniversityBanner = () => {
           </SwiperSlide>
           <SwiperSlide className="swiper-slide">
             <div
+              className="rbt-banner-area rbt-banner-6 variation-03 bg_image bg_image--14-1"
+              onClick={() => {
+                router.push("/placement-records");
+                }}
+                style={{ cursor: "pointer" }}
+            >
+              <div className="wrapper w-100">
+                <div className="container">
+                  <div className="row align-items-center">
+                    <div className="col-lg-12">
+                      <div className="inner text-center">
+                        <div className="section-title">
+                          {/* <span className="subtitle bg-white-opacity d-inline-block">
+                          Lorem ipsum dolor sit amet
+                          </span> */}
+                        </div>
+                        <h1 className="title w-700">
+                        {/* Suspendisse eu arcu massa <br />{" "}
+                          <strong className="color-white">
+                          condimentum ipsum.
+                          </strong> */}
+                        </h1>
+                        <div className="button-group mt--30">
+                         {/*   <Link
+                            className="rbt-btn btn-gradient rbt-marquee-btn"
+                            href="#"
+                          >
+                           <span data-text="More About University">
+                             Know More
+                            </span> 
+                          </Link>*/}
+                        </div>
+                    
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="swiper-slide">
+            <div
+              className="rbt-banner-area rbt-banner-6 variation-03 bg_image bg_image--14-2"
+              onClick={() => {
+                router.push("/placement-records");
+                }}
+                style={{ cursor: "pointer" }}
+            >
+              <div className="wrapper w-100">
+                <div className="container">
+                  <div className="row align-items-center">
+                    <div className="col-lg-12">
+                      <div className="inner text-center">
+                        <div className="section-title">
+                          {/* <span className="subtitle bg-white-opacity d-inline-block">
+                          Lorem ipsum dolor sit amet
+                          </span> */}
+                        </div>
+                        <h1 className="title w-700">
+                        {/* Suspendisse eu arcu massa <br />{" "}
+                          <strong className="color-white">
+                          condimentum ipsum.
+                          </strong> */}
+                        </h1>
+                        <div className="button-group mt--30">
+                         {/*   <Link
+                            className="rbt-btn btn-gradient rbt-marquee-btn"
+                            href="#"
+                          >
+                           <span data-text="More About University">
+                             Know More
+                            </span> 
+                          </Link>*/}
+                        </div>
+                    
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="swiper-slide">
+            <div
               className="rbt-banner-area rbt-banner-6 variation-03 bg_image bg_image--15"
+              onClick={() => {
+                router.push("/placement-records");
+                }}
+                style={{ cursor: "pointer" }}
+            >
+              <div className="wrapper w-100">
+                <div className="container">
+                  <div className="row align-items-center">
+                    <div className="col-lg-12">
+                      <div className="inner text-center">
+                        <div className="section-title">
+                          {/* <span className="subtitle bg-white-opacity d-inline-block">
+                          Lorem ipsum dolor sit amet
+                          </span> */}
+                        </div>
+                        <h1 className="title w-700">
+                        {/* Suspendisse eu arcu massa <br />{" "}
+                          <strong className="color-white">
+                          condimentum ipsum.
+                          </strong> */}
+                        </h1>
+                        <div className="button-group mt--30">
+                         {/*   <Link
+                            className="rbt-btn btn-gradient rbt-marquee-btn"
+                            href="#"
+                          >
+                           <span data-text="More About University">
+                             Know More
+                            </span> 
+                          </Link>*/}
+                        </div>
+                    
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="swiper-slide">
+            <div
+              className="rbt-banner-area rbt-banner-6 variation-03 bg_image bg_image--15-1"
+              onClick={() => {
+                router.push("/placement-records");
+                }}
+                style={{ cursor: "pointer" }}
+            >
+              <div className="wrapper w-100">
+                <div className="container">
+                  <div className="row align-items-center">
+                    <div className="col-lg-12">
+                      <div className="inner text-center">
+                        <div className="section-title">
+                          {/* <span className="subtitle bg-white-opacity d-inline-block">
+                          Lorem ipsum dolor sit amet
+                          </span> */}
+                        </div>
+                        <h1 className="title w-700">
+                        {/* Suspendisse eu arcu massa <br />{" "}
+                          <strong className="color-white">
+                          condimentum ipsum.
+                          </strong> */}
+                        </h1>
+                        <div className="button-group mt--30">
+                         {/*   <Link
+                            className="rbt-btn btn-gradient rbt-marquee-btn"
+                            href="#"
+                          >
+                           <span data-text="More About University">
+                             Know More
+                            </span> 
+                          </Link>*/}
+                        </div>
+                    
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="swiper-slide">
+            <div
+              className="rbt-banner-area rbt-banner-6 variation-03 bg_image bg_image--15-2"
               onClick={() => {
                 router.push("/placement-records");
                 }}
@@ -207,91 +375,7 @@ const UniversityBanner = () => {
             </div>
           </SwiperSlide>
 
-          <SwiperSlide className="swiper-slide">
-            <div
-              className="rbt-banner-area rbt-banner-6 variation-03 bg_image bg_image--18"
-              onClick={() => {
-                router.push("/placement-records");
-                }}
-                style={{ cursor: "pointer" }}
-            >
-              <div className="wrapper w-100">
-                <div className="container">
-                  <div className="row align-items-center">
-                    <div className="col-lg-12">
-                      <div className="inner text-center">
-                        <div className="section-title">
-                          {/* <span className="subtitle bg-white-opacity d-inline-block">
-                          Lorem ipsum dolor sit amet
-                          </span> */}
-                        </div>
-                        <h1 className="title w-700">
-                        {/* Mauris elementum mauris eget  <br />{" "}
-                          <strong className="color-white">
-                          augue ultrices.
-                          </strong> */}
-                        </h1>
-                        <div className="button-group mt--30">
-                          {/*  <Link
-                            className="rbt-btn btn-gradient rbt-marquee-btn radius-round"
-                            href="#"
-                          >
-                           <span data-text="More About University">
-                              Know More
-                            </span> 
-                          </Link>*/}
-                        </div>
-                 
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide className="swiper-slide">
-            <div
-              className="rbt-banner-area rbt-banner-6 variation-03 bg_image bg_image--16"
-              onClick={() => {
-                router.push("/placement-records");
-                }}
-                style={{ cursor: "pointer" }}
-            >
-              <div className="wrapper w-100">
-                <div className="container">
-                  <div className="row align-items-center">
-                    <div className="col-lg-12">
-                      <div className="inner text-center">
-                        <div className="section-title">
-                          {/* <span className="subtitle bg-white-opacity d-inline-block">
-                            THE BEST TEMPLATE FOR EDUCATION
-                          </span> */}
-                        </div>
-                        <h1 className="title w-700">
-                          {/* Learning keeps you <br />{" "}
-                          <strong className="color-white">in the lead</strong> */}
-                        </h1>
-                        <div className="button-group mt--30">
-                          {/*  <Link
-                            className="rbt-btn btn-gradient rbt-marquee-btn radius-round"
-                            href="#"
-                          >
-                           <span data-text="More About University">
-                              More About University
-                            </span> 
-                          </Link>*/}
-                        </div>
-                        <div className="social-share-wrapper mt--40">
-                      
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
+          
         </div>
         <div className="rbt-swiper-arrow rbt-arrow-left">
           <div className="custom-overfolow">
